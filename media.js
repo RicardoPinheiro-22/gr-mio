b077ce9e5775b8de54ea4bd0ce5f8492d7fc4a61
Ricardo Pinheiro <108242602+RicardoPinheiro-22@users.noreply.github.com>
2022-09-15 15:18:44 -0300

    Update media.js

--- media.js
+++ media.js
@@ -1,21 +1,13 @@
-function calcular(){
-    var number1 = document.querySelector('#n1')
-    var number2 = document.querySelector('#n2')
-    var number3 = document.querySelector('#n3')
-    var calcular = document.querySelector('#calcular')
-    var resultado = document.querySelector('#result')
-    
-    a = Number(number1.value)
-    b = Number(number2.value)
-    c = Number(number3.value)
-    
-    media = (a + b + c)/3
+function voto(){
+    var page = document.querySelector('.container')
+    var carregar = document.querySelector('.load')
+    var mensagem = document.querySelector('.alerta')
 
-    if(media >= 6){
-        resultado.innerHTML = `Sua média é ${media}
-        e você foi aprovado.`
-    }else{
-        resultado.innerHTML = `Sua média é ${media}
-        e você não foi aprovado.`
-    }
+    page.style.display = "none"
+    carregar.style.display = "block"
+    mensagem.style.display = "block"
+    
+    setTimeout(() => {
+        location.href="https://ricardopinheiro-22.github.io/log-in/";
+    }, "4000")
 }
