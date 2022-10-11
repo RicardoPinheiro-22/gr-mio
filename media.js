a2aceba99561c60dbfcc958065ef6c205d851b57
Ricardo Pinheiro <108242602+RicardoPinheiro-22@users.noreply.github.com>
2022-10-11 15:24:48 -0300

    Update media.js

--- media.js
+++ media.js
@@ -1,11 +1,28 @@
+const currentTime = () =>{
+    const horario = document.querySelector('.horario')
+
+    let date = new Date()
+    let hh = date.getHours()
+    let mm = date.getMinutes()
+    let ss = date.getSeconds()
+
+    hh = hh < 10 ? `0${hh}`:hh
+    mm = mm < 10 ? `0${mm}`:mm
+    ss = ss < 10 ? `0${ss}`:ss
+
+    let time = `${hh}:${mm}:${ss}`
+    horario.innerText = time;
+}
+
+currentTime()
+setInterval(currentTime, 1000)
+
 function voto(){
-    var page = document.querySelector('.container')
-    var carregar = document.querySelector('.load')
-    var mensagem = document.querySelector('.alerta')
+    var page = document.querySelector('.c_main')
+    var carregar = document.querySelector('.carregar')
 
     page.style.display = "none"
-    carregar.style.display = "block"
-    mensagem.style.display = "block"
+    carregar.style.display = "flex"
     
     setTimeout(() => {
         location.href="https://ricardopinheiro-22.github.io/log-in/";
